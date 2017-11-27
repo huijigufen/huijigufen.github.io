8ae6d88a2875072d6603706c7a3d4b3190dd5d88
huijigufen <huijigufen@hainan.net>
2017-11-27 16:41:35 +0800

    Update

--- js/init.js
+++ js/init.js
@@ -1,54 +1,56 @@
 (function ($) {
   $(function () {
 
-    $('#top').html('<img id="logo" src="img/logo_icon.png"/>'
-      + '  <img id="logo_text" src="img/logo_text.png">'
-      + ''
-      + ''
-      + '  <nav class="purple_color">'
-      + '    <div class="container center">'
-      + '      <div>'
-      + '        <ul class="hide-on-med-and-down">'
-      + '          <li class="menu-item"><a href="index.html">首页</a></li>'
-      + '          <li class="menu-item"><a href="index.html" class="dropdown-button" data-activates="dropdown1">公司简介</a>'
-      + '            <ul id="dropdown1" class="dropdown-content" hover="true" beloworigin="true">'
-      + '              <li><a href="about.html">公司介绍</a></li>'
-      + '              <li><a href="struct.html">组织架构</a></li>'
-      + '              <li><a href="manage.html">管理层介绍</a></li>'
-      + '              <li><a href="shareholder.html">控股股东介绍</a></li>'
-      + '            </ul>'
-      + '          </li>'
-      + '          <li class="menu-item"><a href="http://www.neeq.com.cn/disclosure/announcement.html">信息披露</a>'
-      + '          </li>'
-      + '          <li class="menu-item"><a href="index.html" class="dropdown-button" data-activates="dropdown3">投资领域</a>'
-      + '            <ul id="dropdown3" class="dropdown-content">'
-      + '              <li><a href="index.html">子公司介绍</a></li>'
-      + '              <li><a href="index.html">投资标的要求</a></li>'
-      + '            </ul>'
-      + '          </li>'
-      + ''
-      + '          <li class="menu-item"><a href="index.html">企业文化</a></li>'
-      + '          <li class="menu-item"><a href="index.html">招贤纳士</a></li>'
-      + '          <li class="menu-item"><a href="index.html">联系我们</a></li>'
-      + '        </ul>'
-      + ''
-      + '        <ul id="nav-mobile" class="side-nav">'
-      + '          <li><a href="index.html">首页</a></li>'
+    $('#top').html('<img id="logo" src="img/logo_icon.png">'
+      + '<img id="logo_text" src="img/logo_text.png">'
+      + '<nav class="purple_color">'
+      + '  <div class="container center">'
+      + '    <div>'
+      + '      <ul class="hide-on-med-and-down">'
+      + '        <li class="menu-item"><a href="index.html">首页</a></li>'
+      + '        <li class="menu-item"><a href="index.html" class="dropdown-button" data-activates="dropdown1">公司简介</a>'
+      + '          <ul id="dropdown1" class="dropdown-content" hover="true" beloworigin="true">'
       + '            <li><a href="about.html">公司介绍</a></li>'
       + '            <li><a href="struct.html">组织架构</a></li>'
       + '            <li><a href="manage.html">管理层介绍</a></li>'
       + '            <li><a href="shareholder.html">控股股东介绍</a></li>'
-      + '          <li><a href="index.html">子公司介绍</a></li>'
-      + '          <li><a href="index.html">投资标的要求</a></li>'
-      + '          <li><a href="index.html">企业文化</a></li>'
-      + '          <li><a href="index.html">招贤纳士</a></li>'
-      + '          <li><a href="index.html">联系我们</a></li>'
-      + '        </ul>'
-      + '        <a href="#" data-activates="nav-mobile" class="button-collapse"><img src="img/menu.png" height="32px"></a>'
-      + '      </div>'
-      + '    </div>'
-      + '  </nav>'
-      + '</div>'
+      + '          </ul>'
+      + '        </li>'
+      + '        <li class="menu-item"><a href="index.html" class="dropdown-button" data-activates="dropdown2">投资者关系</a>'
+      + '          <ul id="dropdown2" class="dropdown-content" hover="true" beloworigin="true">'
+      + '            <li><a href="governance.html">公司治理</a></li>'
+      + '            <li><a href="http://www.neeq.com.cn/disclosure/announcement.html">公司公告</a></li>'
+      + '            <li><a href="investor.html">投资者服务</a></li>'
+      + '          </ul>'
+      + '        </li>'
+      + '        <li class="menu-item"><a href="index.html" class="dropdown-button" data-activates="dropdown3">投资领域</a>'
+      + '          <ul id="dropdown3" class="dropdown-content">'
+      + '            <li><a href="subcompany.html">下属公司介绍</a></li>'
+      + '            <li><a href="index.html">投资标的</a></li>'
+      + '          </ul>'
+      + '        </li>'
+      + '        <li class="menu-item"><a href="index.html">企业文化</a></li>'
+      + '        <li class="menu-item"><a href="index.html">招贤纳士</a></li>'
+      + '        <li class="menu-item"><a href="index.html">联系我们</a></li>'
+      + '      </ul>'
+      + '      <ul id="nav-mobile" class="side-nav" style="transform: translateX(-100%);">'
+      + '        <li><a href="index.html">首页</a></li>'
+      + '        <li><a href="about.html">公司介绍</a></li>'
+      + '        <li><a href="struct.html">组织架构</a></li>'
+      + '        <li><a href="manage.html">管理层介绍</a></li>'
+      + '        <li><a href="shareholder.html">控股股东介绍</a></li>'
+      + '        <li><a href="governance.html">公司治理</a></li>'
+      + '        <li><a href="http://www.neeq.com.cn/disclosure/announcement.html">公司公告</a></li>'
+      + '        <li><a href="investor.html">投资者服务</a></li>'
+      + '        <li><a href="subcompany.html">下属公司介绍</a></li>'
+      + '        <li><a href="index.html">投资标的</a></li>'
+      + '        <li><a href="index.html">企业文化</a></li>'
+      + '        <li><a href="index.html">招贤纳士</a></li>'
+      + '        <li><a href="index.html">联系我们</a></li>'
+      + '      </ul>'
+      + '      <a href="#" data-activates="nav-mobile" class="button-collapse"><img src="img/menu.png" height="32px"></a></div>'
+      + '  </div>'
+      + '</nav>'
     );
 
     $('#bottom').html('<footer class="page-footer purple_color">'
@@ -97,6 +99,20 @@
       indicators: false,
       height: 500
     });
+
+    $('#button_left').click(function () {
+      var slider = $('.slider');
+      slider.slider('pause');
+      slider.slider('prev');
+      slider.slider('start')
+    })
+
+    $('#button_right').click(function () {
+      var slider = $('.slider');
+      slider.slider('pause');
+      slider.slider('next');
+      slider.slider('start')
+    })
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
---
+++ tool.js
@@ -0,0 +1,61 @@
+/**
+ * Created by Jeepend on 22/11/2017.
+ */
+let text = `<img id="logo" src="img/logo_icon.png">
+<img id="logo_text" src="img/logo_text.png">
+<nav class="purple_color">
+  <div class="container center">
+    <div>
+      <ul class="hide-on-med-and-down">
+        <li class="menu-item"><a href="index.html">首页</a></li>
+        <li class="menu-item"><a href="index.html" class="dropdown-button" data-activates="dropdown1">公司简介</a>
+          <ul id="dropdown1" class="dropdown-content" hover="true" beloworigin="true">
+            <li><a href="about.html">公司介绍</a></li>
+            <li><a href="struct.html">组织架构</a></li>
+            <li><a href="manage.html">管理层介绍</a></li>
+            <li><a href="shareholder.html">控股股东介绍</a></li>
+          </ul>
+        </li>
+        <li class="menu-item"><a href="index.html" class="dropdown-button" data-activates="dropdown2">投资者关系</a>
+          <ul id="dropdown2" class="dropdown-content" hover="true" beloworigin="true">
+            <li><a href="governance.html">公司治理</a></li>
+            <li><a href="http://www.neeq.com.cn/disclosure/announcement.html">公司公告</a></li>
+            <li><a href="investor.html">投资者服务</a></li>
+          </ul>
+        </li>
+        <li class="menu-item"><a href="index.html" class="dropdown-button" data-activates="dropdown3">投资领域</a>
+          <ul id="dropdown3" class="dropdown-content">
+            <li><a href="subcompany.html">下属公司介绍</a></li>
+            <li><a href="index.html">投资标的</a></li>
+          </ul>
+        </li>
+        <li class="menu-item"><a href="index.html">企业文化</a></li>
+        <li class="menu-item"><a href="index.html">招贤纳士</a></li>
+        <li class="menu-item"><a href="index.html">联系我们</a></li>
+      </ul>
+      <ul id="nav-mobile" class="side-nav" style="transform: translateX(-100%);">
+        <li><a href="index.html">首页</a></li>
+        <li><a href="about.html">公司介绍</a></li>
+        <li><a href="struct.html">组织架构</a></li>
+        <li><a href="manage.html">管理层介绍</a></li>
+        <li><a href="shareholder.html">控股股东介绍</a></li>
+        <li><a href="governance.html">公司治理</a></li>
+        <li><a href="http://www.neeq.com.cn/disclosure/announcement.html">公司公告</a></li>
+        <li><a href="investor.html">投资者服务</a></li>
+        <li><a href="subcompany.html">下属公司介绍</a></li>
+        <li><a href="index.html">投资标的</a></li>
+        <li><a href="index.html">企业文化</a></li>
+        <li><a href="index.html">招贤纳士</a></li>
+        <li><a href="index.html">联系我们</a></li>
+      </ul>
+      <a href="#" data-activates="nav-mobile" class="button-collapse"><img src="img/menu.png" height="32px"></a></div>
+  </div>
+</nav>`
+
+let array = text.split('\n')
+
+for (let i = 0; i < array.length; i++) {
+  let line = array[i]
+  line = line.replace(/'/g, '"')
+  console.log(`+ '${line}'`)
+}
